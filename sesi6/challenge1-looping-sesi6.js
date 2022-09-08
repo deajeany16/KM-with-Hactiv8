7f43ea5e363d6c79e0552043a723c9529d3f7dde
deajeany16 <cydjeany1601@gmail.com>
2022-09-08 15:48:17 +0700

    Menambahkan Folder Baru

---
+++ sesi6/challenge1-looping-sesi6.js
@@ -0,0 +1,10 @@
+let orang = 1
+
+while (orang <= 20) {
+    console.log("Halo, saya orang ke ", orang)
+    orang++
+}
+
+for(let orang=1; orang<=20; orang++){
+    console.log("Halo, saya orang ke ", orang)
+}
---
+++ sesi6/challenge2-looping-sesi6.js
@@ -0,0 +1,17 @@
+for(let i=1; i <= 100; i++){
+    if (i % 2 == 1 ) {
+        console.log(i)
+    }
+}
+
+for(let i=1; i <= 100; i++){
+    if (i % 2 == 0 ) {
+        console.log(i)
+    }
+}
+
+let angka = ""
+for(let i=1; i <= 7; i++){
+    angka = String(i)
+}
+console.log(angka)
